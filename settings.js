d2ec987d226fdd154a17e9fc26cf15bb766c10cd
XENO SIR <vincenzoexe6@gmail.com>
2026-01-20 19:21:57 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'XENO MD',
+  author: '‎',
+  botName: "XENO MD BOT",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '918136810956', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
